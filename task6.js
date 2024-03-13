4e67c0e5f63d780b34eeee04f2b4a1f0a8932c99
Nimrajarral <nimrajarral@gmail.com>
2024-03-14 01:15:31 +0500

    first commit

---
+++ task6.js
@@ -0,0 +1,12 @@
+// Question No 6 :Stripping Name 
+//Store a person name and include some whitespace characters at the beigning and the end of 
+// the name.Use "\t(tab)"and "\n(new line)" at once.Print the name once ,so the whitespace 
+//around the name is displayed.Then print the name after stripping the white spaces.
+// let person_name:string ="Nimra Asif";
+// let caste:string="Jarral";
+// console.log("caste");
+// console.log(`\t\n ${person_name}\t\n`);
+// console.log(`\tcaste`)
+var person_name = "\t\n   Nimra Asif   \t\n";
+console.log(person_name);
+console.log(person_name.trim());
---
+++ task6.ts
@@ -0,0 +1,12 @@
+    // Day 2 Challange:Start Coding
+        // Question No 6 :Stripping Name 
+//Store a person name and include some whitespace characters at the beigning and the end of 
+// the name.Use "\t(tab)"and "\n(new line)" at once.Print the name once ,so the whitespace 
+//around the name is displayed.Then print the name after stripping the white spaces.
+
+// This serve the name with whitespace//
+let person_name:string ="\t\n   Nimra Asif\t\n";
+//Shows the name with white space 
+console.log(person_name);
+//Shows the name without whitespace 
+console.log(person_name.trim());
